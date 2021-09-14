1ea455e24aea9d5757889ec9a9d3e2d11c238282
Sai Sindhur Malleni <smalleni@redhat.com>
2021-09-14 14:46:49 -0400

    Add script that deletes resources without deleting namespaces (#2)
    Signed-off-by: Sai Sindhur Malleni <smalleni@redhat.com>

---
+++ delete_resources_not_namespaces.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+    echo "Pass kube-burner UUID to script"
+    exit 1
+fi
+cat << EOF > cfg_del.yml
+jobs:
+  - name: remove-objects
+    jobType: delete
+    objects:
+      - kind: Deployment
+        labelSelector: {kube-burner-uuid: ${1}}
+        apiVersion: apps/v1
+
+      - kind: Pod
+        labelSelector: {kube-burner-uuid: ${1}}
+
+      - kind: Secret
+        labelSelector: {kube-burner-uuid: ${1}}
+
+      - kind: ConfigMap
+        labelSelector: {kube-burner-uuid: ${1}}
+
+      - kind: Service
+        labelSelector: {kube-burner-uuid: ${1}}
+EOF
+kube-burner init -c cfg_del.yml
+
