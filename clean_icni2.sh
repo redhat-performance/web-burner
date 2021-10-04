5b73a6b838d687a85ebb1e0be6789b90646aa61c
Murali Krishnasamy <mukrishn@redhat.com>
2021-10-04 15:07:07 -0400

    cleanedup templates

--- clean_icni2.sh
+++
@@ -1,1 +0,0 @@
-for j in {1..17}; do for i in $(oc get pods -A| grep -i pod-served- | grep -v init| awk '{print $2}' ); do oc delete pod -n $j $i; done; done
--- ver1.sh
+++
@@ -1,5 +0,0 @@
-POD=ovnkube-master-mvs24
-set -x
-oc exec -n openshift-ovn-kubernetes -it $POD -- ovn-nbctl list bfd | grep up | wc -l
-oc exec -n openshift-ovn-kubernetes -it $POD -- ovn-nbctl list bfd | grep worker
-set +x
