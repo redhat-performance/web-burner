517ed3e96b31733ae3b082e8706a059549a33905
José Castillo Lema <josecastillolema@gmail.com>
2023-07-10 10:27:28 +0200

    Remove oc client dependency (#60)

--- create_icni2_workload.sh
+++ create_icni2_workload.sh
@@ -31,7 +31,7 @@ if [ $? -ne 0 ]; then
     sudo tar -xvzf kube-burner.tar.gz -C /usr/local/bin/
 fi
 
-oc create secret generic kubeconfig --from-file=config=$KUBECONFIG --dry-run=client --output=yaml > objectTemplates/secret_kubeconfig.yml
+kubectl create secret generic kubeconfig --from-file=config=$KUBECONFIG --dry-run=client --output=yaml > objectTemplates/secret_kubeconfig.yml
 
 if [ $# -eq 0 ]; then
     echo "Pass kube-burner config"
@@ -40,14 +40,14 @@ fi
 
 pushd ./workload
 
-lb_workers=$(oc get nodes | grep worker-spk | awk '{print $1}') # get all worker-spk nodes
+lb_workers=$(kubectl get nodes | grep worker-spk | awk '{print $1}') # get all worker-spk nodes
 lb_workers=($lb_workers)
 # Need at least 4 worker-spk nodes for spk pods
 if [[ ${#lb_workers[@]} -ge $lb_count ]] ; then
   echo "Found enough worker-spk nodes for spk pods"
 else
   echo "Not enough worker-spk nodes, labeling nodes"
-  all_workers=$(oc get nodes | grep worker | grep -v worker-lb | grep -v worker-rt | awk '{print $1}') # get all worker nodes except worker-lb and worker-rt
+  all_workers=$(kubectl get nodes | grep worker | grep -v worker-lb | grep -v worker-rt | awk '{print $1}') # get all worker nodes except worker-lb and worker-rt
   all_workers=($all_workers)
   if [[ $lb_count-${#lb_workers[@]} -gt ${#all_workers[@]} ]]; then
       echo "Not enough nodes to label" # there should be enough unlabelled nodes to label them
@@ -56,40 +56,42 @@ else
   count=0 
   while [[ $count -le $lb_count-1 ]]; do
     echo "Label worker nodes.."
-    oc label node ${all_workers[$count]} node-role.kubernetes.io/worker-spk="" --overwrite=true
+    kubectl label node ${all_workers[$count]} node-role.kubernetes.io/worker-spk="" --overwrite=true
     count=$((count+1))
   done
 fi
 
-# Find the right SR-IOV PF
-sriov_nic=""
-for i in $(oc get nodes | grep worker-spk | awk '{print $1}')
-do 
-  nic=$(ssh -i /home/kni/.ssh/id_rsa -o StrictHostKeyChecking=no core@$i "sudo ovs-vsctl list-ports br-ex | head -1")
-  if [[ $sriov_nic == "" ]]; then
-    echo "Setting SR-IOV PF to $nic"
-    export sriov_nic=$nic
-  elif [[ $sriov_nic != $nic ]]; then
-    echo "SR-IOV PF is not matching with other worker-spk nodes"
-    exit 1
-  fi
-done
+if $SRIOV ; then
+  # Find the right SR-IOV PF
+  sriov_nic=""
+  for i in $(kubectl get nodes | grep worker-spk | awk '{print $1}')
+  do 
+    nic=$(ssh -i /home/kni/.ssh/id_rsa -o StrictHostKeyChecking=no core@$i "sudo ovs-vsctl list-ports br-ex | head -1")
+    if [[ $sriov_nic == "" ]]; then
+      echo "Setting SR-IOV PF to $nic"
+      export sriov_nic=$nic
+    elif [[ $sriov_nic != $nic ]]; then
+      echo "SR-IOV PF is not matching with other worker-spk nodes"
+      exit 1
+    fi
+  done
 
-# Applying SR-IOV Network Node policy
-echo "Creating SRIOVNetworkNodePolicy.."
-envsubst < sriov_policy.yml | oc apply -f -
-sleep 60 # sleep for 60 seconds before checking for status
+  # Applying SR-IOV Network Node policy
+  echo "Creating SRIOVNetworkNodePolicy.."
+  envsubst < sriov_policy.yml | kubectl apply -f -
+  sleep 60 # sleep for 60 seconds before checking for status
 
-echo "Waiting for the worker-spk node to be ready.."
-for mcp in $(oc get mcp --no-headers | awk '{print $1}')
-do 
-  oc wait --for=condition=Updated --timeout=3600s mcp $mcp
-  echo "Nice! $mcp is updated"
-done
+  echo "Waiting for the worker-spk node to be ready.."
+  for mcp in $(kubectl get mcp --no-headers | awk '{print $1}')
+  do 
+    kubectl wait --for=condition=Updated --timeout=3600s mcp $mcp
+    echo "Nice! $mcp is updated"
+  done
+fi
 
 echo "Set Openshift monitoring vars.."
-prometheus_url=$(oc get routes -n openshift-monitoring prometheus-k8s --no-headers | awk '{print $2}')
-token=$(oc create token -n openshift-monitoring prometheus-k8s --duration=6h || oc sa new-token -n openshift-monitoring prometheus-k8s)
+prometheus_url=$(kubectl get routes -n openshift-monitoring prometheus-k8s --no-headers | awk '{print $2}')
+token=$(kubectl create token -n openshift-monitoring prometheus-k8s --duration=6h)
 
 popd
 
